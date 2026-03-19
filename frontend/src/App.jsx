d374642e58c763f6f3721e90e5b685b9476f0b02
Subhajit Ghosh <gamingsubha07@gmail.com>
2026-03-20 00:59:32 +0530

    design home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,27 @@
-import { Show, SignInButton, SignOutButton, UserButton, useUser } from '@clerk/react'
+import { SignInButton, SignOutButton, UserButton, useUser } from '@clerk/react'
 import './App.css'
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import ProblemPage from './pages/ProblemPage';
+import Dashboard from './pages/Dashboard';
 import { Toaster } from 'react-hot-toast';
 
 
 function App() {
 
-  const {isSignedIn,isLoaded} = useUser()
+  const { isSignedIn, isLoaded } = useUser()
+
+  if (!isLoaded) return null // ⏳ wait for Clerk before rendering any route
 
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Home />} />
-            <Route 
-                path="/problems" 
-                element={
-                  !isLoaded ? null  // ⏳ still loading — render nothing (or a spinner)
-                  : isSignedIn ? <ProblemPage /> 
-                  : <Navigate to="/" />
-                } 
-            />
-    </Routes>
+      <Routes>
+        <Route path="/" element={!isSignedIn ? <Home /> : <Navigate to="/dashboard" />} />
+        <Route path="/dashboard" element={isSignedIn ? <Dashboard /> : <Navigate to="/" />} />
+        <Route path="/problems" element={isSignedIn ? <ProblemPage /> : <Navigate to="/" />} />
+      </Routes>
 
-    <Toaster position='top-right' toastOptions={{duration:1500}}/>
+      <Toaster position='top-right' toastOptions={{ duration: 1500 }} />
     </>
   )
 }
