43cdd88f97c023816be9459559370f986e8aa821
Subhajit Ghosh <gamingsubha07@gmail.com>
2026-03-19 22:23:28 +0530

    frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,24 @@
-import { Show, SignInButton, SignOutButton, UserButton } from '@clerk/react'
+import { Show, SignInButton, SignOutButton, UserButton, useUser } from '@clerk/react'
 import './App.css'
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
+import ProblemPage from './pages/ProblemPage';
+import { Toaster } from 'react-hot-toast';
 
 
 function App() {
 
+  const {isSignedIn} = useUser()
+
   return (
-    <div>
-      <h1>welcome</h1>
+    <>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/problems" element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />} />
+    </Routes>
 
-      <Show when="signed-out">
-          <SignInButton mode='modal'/>
-      </Show>
-      <Show when="signed-in">
-          <SignOutButton/>
-      </Show>
-      <Show when="signed-in">
-          <UserButton />
-      </Show>
-
-    </div>
+    <Toaster position='top-right' toastOptions={{duration:1500}}/>
+    </>
   )
 }
 
---
+++ frontend/src/pages/ProblemPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const ProblemPage = () => {
+  return (
+    <div>
+      its the problem page here
+    </div>
+  )
+}
+
+export default ProblemPage
