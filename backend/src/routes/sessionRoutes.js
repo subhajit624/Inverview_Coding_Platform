0c6884162e74cdd1f6d575353e476d23c592046e
Subhajit Ghosh <gamingsubha07@gmail.com>
2026-03-19 21:14:54 +0530

    minor, change get to post

--- backend/src/routes/sessionRoutes.js
+++ backend/src/routes/sessionRoutes.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { protectedRoute } from "../middleware/protectedRoute.js";
-import { get } from "mongoose";
 import { createSession, endSession, getActiveSessions, getMyRecentSessions, getSessionById, joinSession } from "../controllers/sessionController.js";
 
 const router = express.Router();
@@ -9,7 +8,7 @@ router.post("/", protectedRoute, createSession);
 router.get("/active", protectedRoute, getActiveSessions);
 router.get("/my-recent", protectedRoute, getMyRecentSessions);
 router.get("/:id", protectedRoute, getSessionById);
-router.get("/:id/join", protectedRoute, joinSession);
-router.get("/:id/end", protectedRoute, endSession);
+router.post("/:id/join", protectedRoute, joinSession);
+router.post("/:id/end", protectedRoute, endSession);
 
 export default router;
